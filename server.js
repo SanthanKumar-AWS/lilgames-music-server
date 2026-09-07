333af54c0bfcacf8d4a6dcf041adc7c47264502c
santhan <santhankumar.boyana@mphasis.com>
2026-09-07 13:20:53 +0530

    New changes to v3.0 server.js

--- server.js
+++ server.js
@@ -13,7 +13,7 @@ const PORT = process.env.PORT || 3000;
 app.use(express.json());
 
 app.get("/health", (req, res) => {
-    res.status(200).json({ status: "ok",version:"v3" });
+    res.status(200).json({ status: "ok",version:"v3.0" });
 });
 
 const axios = require("axios");
